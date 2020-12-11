5278477bccc073294c48b4a2630c531769ca2e52
Rishu-lko <singhratnesh1989@gmail.com>
2020-12-11 15:25:38 +0530

    Run On Docker

--- src/test/java/com/automation/base/BaseClass.java
+++ src/test/java/com/automation/base/BaseClass.java
@@ -1,9 +1,12 @@
 package com.automation.base;
 
 import java.io.IOException;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -13,16 +16,17 @@ import com.test.utils.Utility;
 
 public class BaseClass {
 
-	protected WebDriver driver = null;
+	protected RemoteWebDriver driver = null;
 	protected PageManagerClass pageObjectManager = null;
 	protected UIOperations uioperations = null;
 	protected PropertyReader propRead = null;
 	protected Utility util = null;
 	@BeforeClass
 	public void init() throws IOException {
-		System.setProperty("webdriver.chrome.driver",
-				System.getProperty("user.dir") + "/src/test/resources/Drivers/chromedriver.exe");
-		driver = new ChromeDriver();
+	//	System.setProperty("webdriver.chrome.driver",
+		//		System.getProperty("user.dir") + "/src/test/resources/Drivers/chromedriver.exe");
+		DesiredCapabilities dc = DesiredCapabilities.chrome();
+		driver = new RemoteWebDriver(new URL("https://localhost:4444/wd/hub"),dc);
 		driver.manage().window().maximize();
 		driver.get(PropertyReader.getInstance().getConfigProperty("url"));
 	}
